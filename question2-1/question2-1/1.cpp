02d8784a22b3b5758f2fd995cf7c3dbf5c999e31
dnjswjd1204 <dnjswjd1204@gmail.com>
2021-08-19 22:05:08 +0900

    commit

---
+++ question2-1/question2-1/1.cpp
@@ -0,0 +1,18 @@
+//#include <iostream>
+//void plus(int& num)
+//{
+//	num++;
+//}
+//void change(int& num)
+//{
+//	num *= -1;
+//}
+//int main()
+//{
+//	int num = 5;
+//	plus(num);
+//	std::cout << num << '\n';
+//	change(num);
+//	std::cout << num;
+//	return 0;
+//}
